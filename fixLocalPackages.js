7fbb4c69ad1e749121416dd77771ea57cecf49af
Ray Deck <ray@raydeck.com>
2020-04-24 07:43:49 -0500

    Fixed reference

--- fixLocalPackages.js
+++ fixLocalPackages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-const fixLocalPackages = require("./fixLocalPackages");
+const fixLocalPackages = require("./");
 const out = fixLocalPackages(process.argv[2] || process.cwd());
 console.log("All done");
--- index.js
+++ index.js
@@ -8,24 +8,26 @@ function fixLocalPackages(path = process.cwd()) {
   const { devDependencies, dependencies } = JSON.parse(
     readFileSync(packagePath, { encoding: "utf8" })
   );
-  Object.entries(devDependencies)
-    .filter(([, path]) => path.startsWith(".") || path.startsWith("/"))
-    .forEach(([key, path]) => {
-      try {
-        removeDependency(key);
-      } catch (e) {}
-      addDevDependency(path, false);
-      fixedDependencies.push({ key, path, isDev: true });
-    });
-  Object.entries(dependencies)
-    .filter(([, path]) => path.startsWith(".") || path.startsWith("/"))
-    .forEach(([key, path]) => {
-      try {
-        removeDependency(key);
-      } catch (e) {}
-      addDependency(path, false);
-      fixedDependencies.push({ key, path, isDev: false });
-    });
+  if (devDependencies)
+    Object.entries(devDependencies)
+      .filter(([, path]) => path.startsWith(".") || path.startsWith("/"))
+      .forEach(([key, path]) => {
+        try {
+          removeDependency(key);
+        } catch (e) {}
+        addDevDependency(path, false);
+        fixedDependencies.push({ key, path, isDev: true });
+      });
+  if (dependencies)
+    Object.entries(dependencies)
+      .filter(([, path]) => path.startsWith(".") || path.startsWith("/"))
+      .forEach(([key, path]) => {
+        try {
+          removeDependency(key);
+        } catch (e) {}
+        addDependency(path, false);
+        fixedDependencies.push({ key, path, isDev: false });
+      });
   return fixedDependencies;
 }
 module.exports = fixLocalPackages;
