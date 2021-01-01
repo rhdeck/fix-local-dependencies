d1dd38e7e884f3cd211e8b68038b372a6e8650d2
Ray Deck <ray@raydeck.com>
2021-01-01 06:07:57 -0500

    feat: clean yarn tmp directories to get better results from resetting local dirs

--- fixLocalPackages.js
+++ fixLocalPackages.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
 const fixLocalPackages = require("./");
+const { spawnSync } = require("child_process");
+const { join } = require("path");
+const { useYarn } = require("yarnif");
+const rimraf = require("rimraf");
+if (useYarn()) {
+  const { stdout } = spawnSync("yarn", ["cache", "dir"]);
+  const cacheDir = stdout;
+  if (cacheDir) {
+    const tmp = join(cacheDir, ".tmp");
+    rimraf.sync(tmp);
+  }
+}
 const out = fixLocalPackages(process.argv[2] || process.cwd());
 console.log("All done");
