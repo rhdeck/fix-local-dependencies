5e0dc9b17a7f08a98262e68991f35aaf850121db
Ray Deck <ray@raydeck.com>
2021-01-01 07:31:31 -0500

    fix: log out the result of yarn cache

--- fixLocalPackages.js
+++ fixLocalPackages.js
@@ -5,11 +5,14 @@ const { join } = require("path");
 const { useYarn } = require("yarnif");
 const rimraf = require("rimraf");
 if (useYarn()) {
-  const { stdout } = spawnSync("yarn", ["cache", "dir"]);
+  const { stdout } = spawnSync("yarn", ["cache", "dir"], { encoding: "utf-8" });
   const cacheDir = stdout.toString();
   if (cacheDir) {
     const tmp = join(cacheDir, ".tmp");
     rimraf.sync(tmp);
+    console.log("Removed all yarn cache contents from ", tmp);
+  } else {
+    console.error("No results from yarn cache dir");
   }
 }
 const out = fixLocalPackages(process.argv[2] || process.cwd());
